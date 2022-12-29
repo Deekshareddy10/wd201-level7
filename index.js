b3f59f2c18189a6fe91711e0228df09cd6554f4b
deeksha <reddydeeksha1006@gmail.com>
2022-12-29 23:06:14 +0530

    all files commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const app = require("./app");
+cconst app = require("./app");
 const port = process.env.PORT || 8742;
 app.listen(port, () => {
-  console.log(`Starte express server at port 3000- ${port}`);
+  console.log(`Started express server at port 3000`);
 });
+
