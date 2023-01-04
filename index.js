5c08ef3bba203a6e000a6540b764eb826cb351b7
deeksha <reddydeeksha1006@gmail.com>
2023-01-04 21:27:05 +0530

    db update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-cconst app = require("./app");
+const app = require("./app");
 const port = process.env.PORT || 8742;
 app.listen(port, () => {
   console.log(`Started express server at port 3000`);
